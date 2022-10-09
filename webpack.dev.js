32152329b78796146302c9cd1e9a80e2a1160156
Vivasvan Patel <vivasvan.patel@k-lens.de>
2022-10-09 21:28:05 +0530

    upgrading webpack version

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,15 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+const path = require("path");
 
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-        // progress: true,
-        liveReload: false
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    // progress: true,
+    liveReload: false,
+    static: {
+      directory: path.join(__dirname),
     },
+  },
 });
